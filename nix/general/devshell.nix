98c762cee925b7b475943fed6380199c1b5d33ed
Rasmus Rendal <r.kjaer@famedly.com>
2026-06-23 18:53:09 +0200

    feat: Include prek packages in default devshells
    This fixes the runtimePkgs required for running our checks not being
    available when the installed hook is executed, and provides an easier
    interface to the individual hooks.

--- nix/general/devshell.nix
+++ nix/general/devshell.nix
@@ -9,6 +9,12 @@ importingFlake: {
     devshells.standards = {
       name = lib.mkDefault "engineering-standards";
 
+      # Include all packages from pre-commit in the shell
+      # - This allows easy access to the individual checks, if developers are interested in running them
+      # - `prek install` doesn't install the wrapped binary, so when executing the hooks, all runtimePkgs
+      #   would be missing if they were not in the shell
+      packages = builtins.map (package: package.data) config.prek-pre-commit.package.runtimePkgs;
+
       commands = [
         {
           name = "prek";
